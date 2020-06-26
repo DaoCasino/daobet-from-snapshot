f30cb551f047a45113a324ee7525449302f641d1
Algys Ievlev <algys.ievlev@gmail.com>
2020-06-26 23:27:34 +0900

    some refactoring

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,8 +10,18 @@ export ENV_NAME=${ENV}
 export DOWNLOAD_PATH='/snapshots'
 DAOBET_SNAPSHOT_PATH=$(python3 snapshot-loader.py --last-load)
 
+# Clear old data if it already exists
+rm -rf ${DAOBET_DATA_DIR}/*
+rm -rf ${DAOBET_SNAPSHOT_PATH}/*
+
+# Create daobet dirs
 mkdir -p ${DAOBET_DATA_DIR}
 mkdir -p ${DAOBET_SNAPSHOT_PATH}
 
 # Start daobet node from snapshot
-exec /usr/bin/daobet-node -d ${DAOBET_DATA_DIR} --config-dir ${DAOBET_CONFIG_DIR} --genesis-json=${DAOBET_CONFIG_DIR}/genesis.json --snapshot=${DAOBET_SNAPSHOT_PATH} --disable-replay-opts
+exec /usr/bin/daobet-node \
+    -d ${DAOBET_DATA_DIR} \
+    --config-dir ${DAOBET_CONFIG_DIR} \
+    --genesis-json=${DAOBET_CONFIG_DIR}/genesis.json \
+    --snapshot=${DAOBET_SNAPSHOT_PATH} \
+    --disable-replay-opts
